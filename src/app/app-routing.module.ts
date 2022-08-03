b85bb680384891acab57195d2159b1d428096904
felipehac <felipehasc@gmail.com>
2022-08-03 03:07:36 -0300

    feat: added cpf masking and validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ScatterTestComponent } from './charts/scatter-test/scatter-test.component';
+import { RouterModule, Routes } from '@angular/router';
+import { GenericFormComponent } from './pages/generic-form/generic-form.component';
+import { RelchartMatrixComponent } from './pages/relchart-matrix/relchart-matrix.component';
 
 const routes: Routes = [
-  { path: 'scatter-test', component: ScatterTestComponent },
+  { path: 'relchart-matrix', component: RelchartMatrixComponent },
+  { path: 'generic-form', component: GenericFormComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { GoogleChartsModule } from 'angular-google-charts';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { TextMaskModule } from 'angular2-text-mask';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScatterTestComponent } from './charts/scatter-test/scatter-test.component';
+import { GenericFormComponent } from './pages/generic-form/generic-form.component';
+import { RelchartMatrixComponent } from './pages/relchart-matrix/relchart-matrix.component';
 
 @NgModule({
-  declarations: [AppComponent, ScatterTestComponent],
-  imports: [BrowserModule, AppRoutingModule, GoogleChartsModule],
+  declarations: [AppComponent, ScatterTestComponent, GenericFormComponent, RelchartMatrixComponent],
+  imports: [BrowserModule, AppRoutingModule, GoogleChartsModule, TextMaskModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
